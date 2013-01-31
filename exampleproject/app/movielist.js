09fca1631846fa0422f41d850807f313d32ca48c
Pål Ruud <ruudud@gmail.com>
2013-01-31 14:51:44 +0100

    Step 2 with router

--- exampleproject/app/movielist.js
+++ exampleproject/app/movielist.js
@@ -1,5 +1,13 @@
-var Movie = Backbone.Model.extend({ });
-var Movies = Backbone.Collection.extend({ model: Movie });
+var Movie = Backbone.Model.extend({
+    idAttribute: 'contentId'
+});
+var Movies = Backbone.Collection.extend({
+    model: Movie,
+
+    parse: function (data) {
+        return data.contentList;
+    }
+});
 
 var List = Backbone.View.extend({
     el: '#movieList',
@@ -13,13 +21,42 @@ var List = Backbone.View.extend({
             var movieView = movieTemplate(movie.toJSON());
             this.$el.append(movieView);
         }.bind(this));
-
+        return this;
     },
 
     _movieTemplate: [
         '<div class="listItem">',
         '  <h2 class="listItemTitle"><%= title %></h2>',
-        '  <img src="<%= coverUrl %>" />',
+        '  <a href="#/movies/<%= contentId %>">',
+        '    <img src="<%= coverUrl %>" />',
+        '  </a>',
         '</div>'
+    ].join('\n')
+});
+
+var MovieView = Backbone.View.extend({
+    el: '#movie',
+    _lastRenderedMovie: null,
+
+    render: function () {
+        if (this.model && this.model != this._lastRenderedMovie) {
+            var template = _.template(this._template);
+
+            this.$el.html(template(this.model.toJSON()));
+        }
+        return this;
+    },
+
+	setModel: function (model) {
+		this.model = model;
+		this.render();
+	},
+
+    _template: [
+		'<div class="inset">',
+        '<h1><%= title %></h1>',
+        '<img src="<%= coverUrl %>" class="floatedLeft" />',
+        '<p><%= shortDescription %></p>',
+		'</div>'
     ].join('\n')
 });
